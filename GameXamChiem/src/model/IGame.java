64826762a9a23c48a536ef8d312478af23dc97d0
NgocDiem <22130038@st.hcmuaf.edu.vn>
2024-12-06 12:51:51 +0700

    updata

--- GameXamChiem/src/model/IGame.java
+++ GameXamChiem/src/model/IGame.java
@@ -5,12 +5,5 @@ import view.Edge;
 import java.util.List;
 
 public interface IGame {
-
-    public List<Node> listChild();
-
-    public boolean isOver();
-
-    public List<Edge> getState();
-
     public void setState(List<Edge> state);
 }
